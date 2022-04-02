ff9c0fd40684d65a8504e63305be36c1fef6c386
Nordine Bittich <nordine@bittich.be>
2022-04-02 06:22:32 +0200

    default invoice template

---
+++ artcoded/src/main/java/tech/artcoded/websitev2/changelogs/$11_DefaultTemplate.java
@@ -0,0 +1,52 @@
+package tech.artcoded.websitev2.changelogs;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import tech.artcoded.websitev2.pages.invoice.InvoiceFreemarkerTemplate;
+import tech.artcoded.websitev2.pages.invoice.InvoiceTemplateRepository;
+import tech.artcoded.websitev2.rest.util.MockMultipartFile;
+import tech.artcoded.websitev2.upload.FileUploadService;
+
+import java.io.IOException;
+
+import static org.apache.commons.io.IOUtils.toByteArray;
+
+@Slf4j
+@ChangeUnit(id = "default-template",
+        order = "11",
+        author = "Nordine Bittich")
+public class $11_DefaultTemplate {
+
+
+    @RollbackExecution
+    public void rollbackExecution() {
+    }
+
+    @Execution
+    public void execute(InvoiceTemplateRepository templateRepository, FileUploadService fileUploadService) throws IOException {
+        if (templateRepository.count() == 0) {
+            var oldTemplate = new ClassPathResource("invoice/template-2021-v2.ftl");
+            InvoiceFreemarkerTemplate ift = InvoiceFreemarkerTemplate.builder()
+                    .name("Template 2021").build();
+
+            log.info("save default invoice template... ");
+            MockMultipartFile templ = MockMultipartFile.builder()
+                    .bytes(toByteArray(oldTemplate.getInputStream()))
+                    .name(oldTemplate.getFilename())
+                    .contentType(MediaType.TEXT_HTML_VALUE)
+                    .originalFilename(oldTemplate.getFilename())
+                    .build();
+            String uploadId = fileUploadService.upload(templ, ift.getId(), false);
+
+            templateRepository.save(ift.toBuilder().templateUploadId(uploadId).build());
+
+
+        }
+
+    }
+
+}
