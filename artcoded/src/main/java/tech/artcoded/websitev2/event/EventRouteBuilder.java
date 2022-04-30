d8cadb365ce67448848d3e478f7a1888398f3b69
Nordine Bittich <nordine@bittich.be>
2022-04-30 12:05:58 +0200

    bugfix

--- artcoded/src/main/java/tech/artcoded/websitev2/event/EventRouteBuilder.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/event/EventRouteBuilder.java
@@ -5,6 +5,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import tech.artcoded.event.IEvent;
 
 import static tech.artcoded.websitev2.api.common.Constants.EVENT_PUBLISHER_SEDA_ROUTE;
 
