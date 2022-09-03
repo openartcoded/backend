f3f284239a74edae8c877691a2ffdc252ba5909f
Nordine Bittich <nordine@bittich.be>
2022-09-03 10:42:57 +0200

    bugfix

--- artcoded/src/main/java/tech/artcoded/websitev2/changelogs/$18_UpdateOptionActionParameter.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/changelogs/$18_UpdateOptionActionParameter.java
@@ -5,6 +5,7 @@ import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
@@ -25,7 +26,7 @@ public class $18_UpdateOptionActionParameter {
   @Execution
   public void execute(MongoTemplate mongoTemplate) throws IOException {
     if (mongoTemplate.collectionExists("reminderTask")) {
-      Query all = new Query();
+      Query all = Query.query(Criteria.where("actionParameters").exists(true));
       mongoTemplate.updateMulti(all, Update.update("actionParameters.$[].options", Map.of()), "reminderTask");
     }
 
