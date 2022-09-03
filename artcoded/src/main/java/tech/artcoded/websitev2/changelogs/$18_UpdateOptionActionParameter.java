df5912af9b2206b84d42f7be82146a5b2b0e674b
Nordine Bittich <nordine@bittich.be>
2022-09-03 06:55:45 +0200

    update options field

---
+++ artcoded/src/main/java/tech/artcoded/websitev2/changelogs/$18_UpdateOptionActionParameter.java
@@ -0,0 +1,44 @@
+package tech.artcoded.websitev2.changelogs;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import tech.artcoded.websitev2.pages.personal.PersonalInfoRepository;
+
+import java.io.IOException;
+
+
+@Slf4j
+@ChangeUnit(id = "update-option-action-parameter",
+  order = "18",
+  author = "Nordine Bittich")
+public class $18_UpdateOptionActionParameter {
+
+  @RollbackExecution
+  public void rollbackExecution() {
+  }
+
+  @Execution
+  public void execute(MongoTemplate mongoTemplate,
+                      PersonalInfoRepository repository) throws IOException {
+    if (mongoTemplate.collectionExists("reminderTask")) {
+     var collection = mongoTemplate.getCollection("reminderTask");
+      Query all = new Query();
+      mongoTemplate.updateMulti(all, Update.update("actionParameters.$[].options", "{}"), "reminderTask");
+
+    }
+    repository.findAll().forEach(personalInfo -> {
+      repository.save(personalInfo.toBuilder()
+        .maxDaysToPay(30)
+        .build());
+    });
+
+
+  }
+
+}
