af115541b129c2f94b3fa30c074d03459aa17ed1
Nordine Bittich <nordine@bittich.be>
2023-04-08 17:20:32 +0200

    only one consumer

--- artcoded/src/main/java/tech/artcoded/websitev2/event/EventAuditLog.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/event/EventAuditLog.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-@Component
-@Slf4j
-@ConditionalOnProperty(name = "application.events.consumerEnabled", havingValue = "true")
+//@Component
+//@Slf4j
+//@ConditionalOnProperty(name = "application.events.consumerEnabled", havingValue = "true")
 public class EventAuditLog extends RouteBuilder {
   @Value("${application.events.topicPublish}")
   private String topicToPublish;
@@ -18,8 +18,8 @@ public class EventAuditLog extends RouteBuilder {
   public void configure() throws Exception {
     log.warn("Event audit log consumer is enabled.");
     fromF("%s", topicToPublish)
-      .routeId("EventAuditLog#consume")
-      .log(LoggingLevel.DEBUG, "event of type '${headers.EventType}' received: ${body}");
+        .routeId("EventAuditLog#consume")
+        .log(LoggingLevel.DEBUG, "event of type '${headers.EventType}' received: ${body}");
   }
 
 }
