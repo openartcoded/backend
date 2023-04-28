3933134c23c26e257011975808d25b2812efa215
Nordine Bittich <nordine@bittich.be>
2023-04-28 10:30:07 +0200

    fix bug in db

---
+++ artcoded/src/main/java/tech/artcoded/websitev2/changelogs/$34_FixPeriodInvoice.java
@@ -0,0 +1,34 @@
+package tech.artcoded.websitev2.changelogs;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import tech.artcoded.websitev2.pages.invoice.InvoiceGenerationRepository;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+
+@ChangeUnit(id = "fix-period-invoice", order = "34", author = "Nordine Bittich")
+public class $34_FixPeriodInvoice {
+
+  @RollbackExecution
+  public void rollbackExecution() {
+  }
+
+  @Execution
+  public void execute(InvoiceGenerationRepository repository) throws IOException {
+    repository.findById("8a83a2cb-f17e-4cf5-a2fd-8699ee22edb9").ifPresent(invoice -> {
+      var row = invoice.getInvoiceTable().get(0);
+      if (row != null && StringUtils.isNotEmpty(row.getPeriod())) {
+        var period = row.getPeriod().split("-");
+        if (period.length == 2) {
+          row.setPeriod(period[1] + "/" + period[0]);
+          repository.save(invoice);
+        }
+      }
+    });
+
+  }
+
+}
