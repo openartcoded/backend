447afdd5f262edf5e0d5a7bd1ad3350962ff6c7d
Nordine Bittich <nordine@bittich.be>
2023-05-31 20:20:49 +0200

    bug default seq

--- artcoded/src/main/java/tech/artcoded/websitev2/changelogs/$36_InitInvoiceSeq.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/changelogs/$36_InitInvoiceSeq.java
@@ -3,6 +3,7 @@ package tech.artcoded.websitev2.changelogs;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
+import tech.artcoded.websitev2.pages.invoice.InvoiceGenerationRepository;
 import tech.artcoded.websitev2.pages.invoice.seq.InvoicePrimarySequenceService;
 
 import java.io.IOException;
@@ -15,8 +16,9 @@ public class $36_InitInvoiceSeq {
   }
 
   @Execution
-  public void execute(InvoicePrimarySequenceService service) throws IOException {
+  public void execute(InvoicePrimarySequenceService service, InvoiceGenerationRepository repo) throws IOException {
     if (service.getCurrent() == null) {
+      repo.findAll().forEach(i -> repo.save(i.toBuilder().seqInvoiceNumber(null).build()));
       service.setValueTo(14);
     }
 
