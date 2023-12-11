4098c255153ae7b69dcd7dca400e82512dd32f0f
Nordine Bittich <nordine@bittich.be>
2023-12-11 21:20:35 +0100

    bump everything

--- artcoded/src/main/java/tech/artcoded/websitev2/pages/toolbox/pdfutils/PdfController.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/pages/toolbox/pdfutils/PdfController.java
@@ -7,8 +7,11 @@ import java.io.File;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
-
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
 import org.apache.commons.io.FileUtils;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessReadBuffer;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
@@ -18,9 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.model.ZipParameters;
 import tech.artcoded.websitev2.utils.helper.IdGenerators;
 
 @RestController
@@ -29,15 +29,17 @@ public class PdfController {
 
   @PostMapping(value = "/split", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<ByteArrayResource> splitPdf(@RequestPart(value = "pdf") MultipartFile pdf) throws Exception {
-    PDDocument document = PDDocument.load(pdf.getInputStream());
+
     File tempZip = new File(getTempDirectory(), pdf.getOriginalFilename() + ".zip");
     ZipParameters zipParameters = new ZipParameters();
     zipParameters.setIncludeRootFolder(false);
     List<File> files = new ArrayList<>();
-    try (var zipFile = new ZipFile(tempZip)) {
+    try (var zipFile = new ZipFile(tempZip);
+        var document = new PDDocument(new COSDocument(),
+            new RandomAccessReadBuffer(pdf.getInputStream()))) {
       for (var page : document.getPages()) {
         var temp = new File(getTempDirectory(), IdGenerators.get() + ".pdf");
-        try(PDDocument doc = new PDDocument()) {
+        try (PDDocument doc = new PDDocument()) {
           doc.addPage(page);
           doc.save(temp);
           files.add(temp);
@@ -45,12 +47,12 @@ public class PdfController {
       }
       zipFile.addFiles(files, zipParameters);
     }
-    var zipBAR = transformToByteArrayResource(tempZip.getName(),
-        URLConnection.guessContentTypeFromName(tempZip.getName()), FileUtils.readFileToByteArray(tempZip));
+    var zipBAR = transformToByteArrayResource(
+        tempZip.getName(),
+        URLConnection.guessContentTypeFromName(tempZip.getName()),
+        FileUtils.readFileToByteArray(tempZip));
     FileUtils.delete(tempZip);
     files.forEach(File::delete);
     return zipBAR;
-
   }
-
 }
