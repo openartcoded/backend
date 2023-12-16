8a6c7af2f198005aaf3cd860b27f0b8d7c538187
Nordine Bittich <nordine@bittich.be>
2023-12-16 08:47:06 +0100

    virtual threads not supported in undertow

--- artcoded/src/main/java/tech/artcoded/websitev2/ArtcodedV2Application.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/ArtcodedV2Application.java
@@ -1,10 +1,13 @@
 package tech.artcoded.websitev2;
 
 import io.mongock.runner.springboot.EnableMongock;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -14,9 +17,6 @@ import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tech.artcoded.websitev2.rest.exception.DefaultExceptionHandler;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableScheduling
@@ -30,6 +30,12 @@ public class ArtcodedV2Application implements AsyncConfigurer {
   @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
   public AsyncTaskExecutor asyncTaskExecutor() {
     return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+  }
+
+  @Bean
+  public UndertowDeploymentInfoCustomizer undertowDeploymentInfoCustomizer() {
+    return deploymentInfo -> deploymentInfo.setExecutor(
+        Executors.newVirtualThreadPerTaskExecutor());
   }
 
   @Override
