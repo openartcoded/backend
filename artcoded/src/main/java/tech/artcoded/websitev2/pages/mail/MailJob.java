c78ee78fa9c4409517d3af84cee8cb84e8c7b914
Nordine Bittich <nordine@bittich.be>
2024-01-25 22:39:18 +0100

    whatever

--- artcoded/src/main/java/tech/artcoded/websitev2/pages/mail/MailJob.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/pages/mail/MailJob.java
@@ -11,6 +11,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 @Document
