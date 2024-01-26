77483d2e9bd8e94948e9e2f84906216f60e84c4a
Nordine Bittich <nordine@bittich.be>
2024-01-26 18:50:20 +0100

    delete and update

--- artcoded/src/main/java/tech/artcoded/websitev2/pages/mail/MailController.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/pages/mail/MailController.java
@@ -7,10 +7,12 @@ import javax.inject.Inject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import tech.artcoded.websitev2.upload.FileUploadService;
@@ -30,6 +32,28 @@ public class MailController {
   @GetMapping("/find-all")
   public Page<MailJob> findAll(Pageable pageable) {
     return this.jobRepository.findByOrderBySendingDateDesc(pageable);
+  }
+
+  @DeleteMapping("/")
+  public void delete(@RequestParam("id") String id) {
+    this.jobRepository.findById(id)
+        .filter(m -> !m.isSent())
+        .ifPresent(mail -> this.jobRepository.deleteById(mail.getId()));
+  }
+
+  @PostMapping("/update")
+  public ResponseEntity<MailJob> update(@RequestBody MailJob mailJob) {
+    return Optional.ofNullable(mailJob.getId())
+        .flatMap(jobRepository::findById)
+        .filter(m -> !m.isSent())
+        .map(m -> m.toBuilder()
+            .subject(mailJob.getSubject())
+            .body(mailJob.getBody())
+            .to(mailJob.getTo())
+            .build())
+        .map(jobRepository::save)
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.status(400).build());
   }
 
   @PostMapping("/send")
