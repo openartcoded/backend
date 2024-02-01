16160bec77d9af90cf5fb5ced5b562936f31b03e
Nordine Bittich <nordine@bittich.be>
2024-02-01 20:25:28 +0100

    fixup default

--- artcoded/src/main/java/tech/artcoded/websitev2/action/CleanupActionResultAction.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/action/CleanupActionResultAction.java
@@ -1,13 +1,12 @@
 package tech.artcoded.websitev2.action;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Component;
-
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
@@ -22,18 +21,19 @@ public class CleanupActionResultAction implements Action {
 
   public static ActionMetadata getDefaultMetadata() {
     return ActionMetadata.builder()
-      .key(ACTION_KEY)
-      .title("Cleanup action results")
-      .description("An action to periodically cleanup action results from previous tasks")
-      .allowedParameters(List.of(ActionParameter.builder()
-        .key(ACTION_PARAMETER_NUMBER_OF_DAYS)
-        .description("Number of days before cleaning it. Default is 30")
-        .parameterType(ActionParameterType.LONG)
-        .required(false)
-        .build()))
-      .defaultCronValue("0 0 0 1 * *")
-      .build();
-
+        .key(ACTION_KEY)
+        .title("Cleanup action results")
+        .description(
+            "An action to periodically cleanup action results from previous tasks")
+        .allowedParameters(List.of(
+            ActionParameter.builder()
+                .key(ACTION_PARAMETER_NUMBER_OF_DAYS)
+                .description("Number of days before cleaning it. Default is 6")
+                .parameterType(ActionParameterType.LONG)
+                .required(false)
+                .build()))
+        .defaultCronValue("0 0 0 1 * *")
+        .build();
   }
 
   @Override
@@ -42,20 +42,24 @@ public class CleanupActionResultAction implements Action {
     List<String> messages = new ArrayList<>();
     try {
       Long daysBefore = parameters.stream()
-        .filter(p -> ACTION_PARAMETER_NUMBER_OF_DAYS.equals(p.getKey()))
-        .filter(p -> StringUtils.isNotEmpty(p.getValue()))
-        .findFirst()
-        .flatMap(p -> p.getParameterType().castLong(p.getValue())).orElse(6L);
+          .filter(p -> ACTION_PARAMETER_NUMBER_OF_DAYS.equals(p.getKey()))
+          .filter(p -> StringUtils.isNotEmpty(p.getValue()))
+          .findFirst()
+          .flatMap(p -> p.getParameterType().castLong(p.getValue()))
+          .orElse(6L);
       Date searchDate = Date.from(ZonedDateTime.now().minusDays(daysBefore).toInstant());
-      messages.add("days before: %s, date to search: %s".formatted(daysBefore, searchDate.toString()));
+      messages.add("days before: %s, date to search: %s".formatted(
+          daysBefore, searchDate.toString()));
       repository.deleteByFinishedDateBefore(searchDate);
       messages.add("after cleaning, count %s".formatted(repository.count()));
       return resultBuilder.finishedDate(new Date()).messages(messages).build();
     } catch (Exception e) {
       log.error("error while executing action", e);
       messages.add("error, see logs: %s".formatted(e.getMessage()));
-      return resultBuilder.finishedDate(new Date()).messages(messages).status(StatusType.FAILURE).build();
-
+      return resultBuilder.finishedDate(new Date())
+          .messages(messages)
+          .status(StatusType.FAILURE)
+          .build();
     }
   }
 
