edf5691680c02331d8d03a096e42a9e695364aa0
Nordine Bittich <contact@bittich.be>
2024-10-13 09:19:04 +0200

    debug mode

--- artcoded/src/main/java/tech/artcoded/websitev2/changelogs/CHANGE_LOG_44_AddUuidExperience.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/changelogs/CHANGE_LOG_44_AddUuidExperience.java
@@ -5,6 +5,8 @@ import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.extern.slf4j.Slf4j;
 import tech.artcoded.websitev2.pages.cv.service.CurriculumService;
 import tech.artcoded.websitev2.utils.helper.IdGenerators;
@@ -21,11 +23,13 @@ public class CHANGE_LOG_44_AddUuidExperience {
   public void execute(CurriculumService cvService)
       throws IOException {
     var cv = cvService.getFullCurriculum();
+    log.info("cv: {}", new ObjectMapper().writeValueAsString(cv));
     cv.getExperiences().forEach(exp -> {
       if (exp.getUuid() == null)
         exp.setUuid(IdGenerators.get());
     });
 
+    log.info("cv after: {}", new ObjectMapper().writeValueAsString(cv));
     cvService.update(cv);
   }
 }
