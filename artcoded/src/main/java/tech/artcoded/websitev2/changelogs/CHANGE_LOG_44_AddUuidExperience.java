cbfc88abefccd5d1247f0fad22addd623bc610ac
Nordine Bittich <contact@bittich.be>
2024-10-13 09:25:59 +0200

    disable arm64 when pushing to main

--- artcoded/src/main/java/tech/artcoded/websitev2/changelogs/CHANGE_LOG_44_AddUuidExperience.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/changelogs/CHANGE_LOG_44_AddUuidExperience.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
+import tech.artcoded.websitev2.pages.cv.repository.CurriculumRepository;
 import tech.artcoded.websitev2.pages.cv.service.CurriculumService;
 import tech.artcoded.websitev2.utils.helper.IdGenerators;
 
@@ -20,9 +21,13 @@ public class CHANGE_LOG_44_AddUuidExperience {
   }
 
   @Execution
-  public void execute(CurriculumService cvService)
+  public void execute(CurriculumRepository curriculumRepository, CurriculumService cvService)
       throws IOException {
-    var cv = cvService.getFullCurriculum();
+    var cv = curriculumRepository.findAll().getFirst();
+    if (cv == null) {
+      log.info("no cv found. continue...");
+      return;
+    }
     log.info("cv: {}", new ObjectMapper().writeValueAsString(cv));
     cv.getExperiences().forEach(exp -> {
       if (exp.getUuid() == null)
