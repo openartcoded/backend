a5d2fc361d48e0314277523cf122b75846df8170
Nordine Bittich <contact@bittich.be>
2025-09-25 23:33:03 +0200

    unnecessary import

--- artcoded/src/main/java/tech/artcoded/websitev2/pages/invoice/InvoiceGenerationController.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/pages/invoice/InvoiceGenerationController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import net.sf.saxon.expr.flwor.Tuple;
 import tech.artcoded.websitev2.peppol.PeppolService;
 import tech.artcoded.websitev2.peppol.PeppolStatus;
 
