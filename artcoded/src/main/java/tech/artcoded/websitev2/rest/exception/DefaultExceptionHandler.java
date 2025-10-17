603bd65f6a4148751aed040e0bc1b89021608bd8
Nordine Bittich <contact@bittich.be>
2025-10-17 22:27:30 +0200

    nfe

--- artcoded/src/main/java/tech/artcoded/websitev2/rest/exception/DefaultExceptionHandler.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/rest/exception/DefaultExceptionHandler.java
@@ -9,34 +9,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
 import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 @Slf4j
 public class DefaultExceptionHandler {
-    private final MailService mailService;
+  private final MailService mailService;
 
-    @Value("${application.admin.email}")
-    private String adminEmail;
+  @Value("${application.admin.email}")
+  private String adminEmail;
 
-    public DefaultExceptionHandler(MailService mailService) {
-        this.mailService = mailService;
+  public DefaultExceptionHandler(MailService mailService) {
+    this.mailService = mailService;
+  }
+
+  @ExceptionHandler({ Exception.class })
+  public ResponseEntity<Map.Entry<String, String>> runtimeException(WebRequest webRequest, Exception exception) {
+    if (exception instanceof NoResourceFoundException nfe) {
+      log.warn("resource {} not found", nfe.getBody());
+      return ResponseEntity.notFound().build();
     }
+    log.error("an error occurred ", exception);
+    Thread.startVirtualThread(() -> {
+      try {
+        log.warn("attempt to send exception by email");
+        mailService.sendMail(List.of(adminEmail), "Artcoded error",
+            "<p>%s</p>".formatted(ExceptionUtils.getStackTrace(exception)), false, List::of);
 
-    @ExceptionHandler({ Exception.class })
-    public ResponseEntity<Map.Entry<String, String>> runtimeException(WebRequest webRequest, Exception exception) {
-        log.error("an error occurred ", exception);
-        Thread.startVirtualThread(() -> {
-            try {
-                log.warn("attempt to send exception by email");
-                mailService.sendMail(List.of(adminEmail), "Artcoded error",
-                        "<p>%s</p>".formatted(ExceptionUtils.getStackTrace(exception)), false, List::of);
-
-            } catch (Exception e) {
-                log.error("could not send email", e);
-            }
-        });
-        return ResponseEntity.badRequest().body(Map.entry("stackTrace", ExceptionUtils.getStackTrace(exception)));
-    }
+      } catch (Exception e) {
+        log.error("could not send email", e);
+      }
+    });
+    return ResponseEntity.badRequest().body(Map.entry("stackTrace", ExceptionUtils.getStackTrace(exception)));
+  }
 }
