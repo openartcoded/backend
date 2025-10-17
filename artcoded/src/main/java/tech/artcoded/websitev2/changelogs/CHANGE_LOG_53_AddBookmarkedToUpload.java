a20c9f71d69a781c3bbd26212599a39870cb38bb
Nordine Bittich <contact@bittich.be>
2025-10-17 18:11:17 +0200

    forgot to save

--- artcoded/src/main/java/tech/artcoded/websitev2/changelogs/CHANGE_LOG_53_AddBookmarkedToUpload.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/changelogs/CHANGE_LOG_53_AddBookmarkedToUpload.java
@@ -4,19 +4,19 @@ package tech.artcoded.websitev2.changelogs;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
-import tech.artcoded.websitev2.pages.fee.FeeRepository;
+import tech.artcoded.websitev2.upload.FileUploadRepository;
 
 import java.io.IOException;
 
-@ChangeUnit(id = "add-bookmarked-to-fee", order = "50", author = "Nordine Bittich")
-public class CHANGE_LOG_50_AddBookmarkedToFee {
+@ChangeUnit(id = "add-bookmarked-to-upload", order = "53", author = "Nordine Bittich")
+public class CHANGE_LOG_53_AddBookmarkedToUpload {
 
   @RollbackExecution
   public void rollbackExecution() {
   }
 
   @Execution
-  public void execute(FeeRepository repository) throws IOException {
+  public void execute(FileUploadRepository repository) throws IOException {
     repository.findAll().stream().map(f -> f.toBuilder().bookmarked(false).build())
         .forEach(repository::save);
 
