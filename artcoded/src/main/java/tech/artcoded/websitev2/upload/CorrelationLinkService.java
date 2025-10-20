33caf7b4601e92710fbacfed3297fe7563f8a4fb
Nordine Bittich <contact@bittich.be>
2025-10-20 17:23:42 +0200

    more logs

--- artcoded/src/main/java/tech/artcoded/websitev2/upload/CorrelationLinkService.java
+++ artcoded/src/main/java/tech/artcoded/websitev2/upload/CorrelationLinkService.java
@@ -7,26 +7,32 @@ import java.util.stream.Collectors;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class CorrelationLinkService {
-    public static final String CACHE_LINKS_KEY = "correlationLinks";
-    private final List<ILinkable> linkables;
-    private final FileUploadService uploadService;
+  public static final String CACHE_LINKS_KEY = "correlationLinks";
+  private final List<ILinkable> linkables;
+  private final FileUploadService uploadService;
 
-    public CorrelationLinkService(List<ILinkable> linkables, FileUploadService uploadService) {
-        this.linkables = linkables;
-        this.uploadService = uploadService;
-    }
+  public CorrelationLinkService(List<ILinkable> linkables, FileUploadService uploadService) {
+    this.linkables = linkables;
+    this.uploadService = uploadService;
+  }
 
-    @CachePut(cacheNames = CACHE_LINKS_KEY, key = "'getLinks'")
-    public Map<String, String> getLinks() {
-        log.debug("linkables size: {}", linkables.size());
-        var correlationIds = uploadService.findAllCorrelationIds();
-        log.debug("correlation ids {}", correlationIds);
-        return linkables.stream().flatMap(linkable -> linkable.getCorrelationLabels(correlationIds).entrySet().stream())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
+  @CachePut(cacheNames = CACHE_LINKS_KEY, key = "'getLinks'")
+  public Map<String, String> getLinks() {
+    var correlationIds = uploadService.findAllCorrelationIds();
+    log.debug("correlation ids {}", correlationIds);
+    return linkables.stream().flatMap(linkable -> linkable.getCorrelationLabels(correlationIds).entrySet().stream())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+  }
+
+  @PostConstruct
+  public void logLinkables() {
+    log.info("linkables size: {}", linkables.size());
+    linkables.stream().forEach(linkable -> log.info("Loaded Linkable {}", linkable.getClass().getSimpleName()));
+  }
 }
